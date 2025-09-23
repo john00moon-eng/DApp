8e832f3f9fdd770686ea9bcb4341065818adddf1
john00moon-eng <john00moon@gmail.com>
2025-09-23 13:27:14 +0300

    Add Pulse indicator dashboard with mock data

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./dapp/пульс/src/**/*.{js,ts,jsx,tsx,html}"],
   theme: {
     extend: {
       colors: {
